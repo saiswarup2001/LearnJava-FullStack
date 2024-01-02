4c217948bf9389b4078734aa77c8a295e9b5a34a
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-01-02 13:55:51 +0530

    for loop

---
+++ Basic/Loops/forloop.java
@@ -0,0 +1,13 @@
+package Basic.Loops;
+
+public class forloop {
+    public static void main(String[] args) {
+        int x = 4;
+        for(int i = 0; i<4; i++){
+            System.out.print(i+" ");
+        }
+    }
+    
+}
+
+// run command -- java .\forloop.java
