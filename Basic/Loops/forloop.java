aba1dd37528ff3894fb885274f80df84da4f3ba8
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-01-03 12:01:37 +0530

    foreach loop

--- Basic/Loops/forloop.java
+++ Basic/Loops/forloop.java
@@ -3,8 +3,15 @@ package Basic.Loops;
 public class forloop {
     public static void main(String[] args) {
         int x = 4;
-        for(int i = 0; i<4; i++){
+        for(int i = 0; i<x; i++){
             System.out.print(i+" ");
+        }
+
+        System.out.println();
+        System.out.println("-------------For Each Loop------------");
+        int y[] = {8,10,20,30};
+        for (int in : y) {
+            System.out.print(in+" ");
         }
     }
     
